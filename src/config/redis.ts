ce4beacc62671ee29c411177a6ea493fac31c540
priyanshpatel18 <=>
2024-08-25 17:03:15 +0530

    redis env variables changed

--- src/config/redis.ts
+++ src/config/redis.ts
@@ -5,8 +5,8 @@ const UPSTASH_REDIS_REST_URL = process.env.UPSTASH_REDIS_REST_URL;
 const UPSTASH_REDIS_REST_TOKEN = process.env.UPSTASH_REDIS_REST_TOKEN;
 
 const redis = new Redis({
-  url: `https://${UPSTASH_REDIS_REST_URL}`,
+  url: UPSTASH_REDIS_REST_URL,
   token: UPSTASH_REDIS_REST_TOKEN,
-});
+})
 
 export default redis;
