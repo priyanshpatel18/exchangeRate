40b123a171b4ccadd1ab9fa3fad033b8981922a5
priyanshpatel18 <=>
2024-08-25 17:14:08 +0530

    added value calculation

--- src/controllers/exchangeRates.ts
+++ src/controllers/exchangeRates.ts
@@ -4,6 +4,7 @@ import { getExchangeRate } from "../services/exchangeRateService";
 export const getExchangeRateController = async (req: Request, res: Response) => {
   // Get Currency from params
   const { currency } = req.params;
+  const value = req.query.value ? Number(req.query.value) : null;
 
   try {
     // Get Exchange Rate from Redis
@@ -11,7 +12,12 @@ export const getExchangeRateController = async (req: Request, res: Response) => 
     if (!exchangeRate) {
       return res.status(404).json({ message: "Invalid Currency" });
     }
-    return res.status(200).json({ currency, data: exchangeRate });
+
+    if (value && !isNaN(value)) {
+      return res.status(200).json({ currency, data: exchangeRate, value: exchangeRate * value });
+    } else {
+      return res.status(200).json({ currency, data: exchangeRate });
+    }
   } catch (error) {
     return res.status(500).json({ message: "Internal Server Error" });
   }
